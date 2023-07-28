9922e50132ac3279ebec0bda6bb58dddd824c042
gilbert2003-beep <gilbert.bageni@student.moringaschool.com>
2023-07-28 19:04:48 +0300

    refactor: Destructure bot object properties in MyBotCard component

--- bot-bottlr/src/components/MyBotCard.js
+++ bot-bottlr/src/components/MyBotCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 const MyBotCard=({bot, actionLabel, actionHandler})=>{
+    const { id, name, health, damage, armor, bot_class, catchphrase, avatar_url } = bot;
+
     return(
         <div className="bot-card">
             <img src="{avatar_url}" alt="{name}" />
