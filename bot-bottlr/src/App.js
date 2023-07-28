d9e65d03ec977183b5e541633bb8218fdda30e5e
gilbert2003-beep <gilbert.bageni@student.moringaschool.com>
2023-07-28 16:45:02 +0300

    feat: Fetch and set bot data from the server

--- bot-bottlr/src/App.js
+++ bot-bottlr/src/App.js
@@ -5,6 +5,15 @@ import './App.css';
 const App=()=>{
   const [bots, setBots]=useState([]);
   const [MyBotArmy, setMyBotArmy]=useState([])
+
+  useEffect(() => {
+    // Fetch data from the server and set it in the state
+    fetch('http://localhost:8001/bots')
+      .then((response) => response.json())
+      .then((data) => setBots(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
     </div>
