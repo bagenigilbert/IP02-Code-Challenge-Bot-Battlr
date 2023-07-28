04d13d62ad0d05b885d0c8d5320d2d4fc9cbe945
gilbert2003-beep <gilbert.bageni@student.moringaschool.com>
2023-07-28 16:38:59 +0300

    Implement EnlistedBots component and update App to use it

--- bot-bottlr/src/App.js
+++ bot-bottlr/src/App.js
@@ -4,7 +4,7 @@ import MyBotArmy from './components/MyBotArmy';
 import './App.css';
 const App=()=>{
   const [bots, setBots]=useState([]);
-  
+  const [MyBotArmy, setMyBotArmy]=useState([])
   return (
     <div className="App">
     </div>
