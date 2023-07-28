48d104544a7b9f0613ee23306b46e030d56652e8
gilbert2003-beep <gilbert.bageni@student.moringaschool.com>
2023-07-28 19:00:41 +0300

    feat: Create MyBotArmy component

--- bot-bottlr/src/components/MyBotArmy.js
+++ bot-bottlr/src/components/MyBotArmy.js
@@ -1,15 +1,15 @@
-// import React from 'react';
-// import BotCard from "./Botcard";
+import React from 'react';
+import BotCard from "./Botcard";
 
-// const MyBotArmy=({bots, dischargeBot})=>{
-//     return(
-//         <div className='your-bot-army'>
-//             <h2>My Bot Army</h2>
-//             {bots.map((bot)=>(
-//         <BotCard key={bot.id} bot={bot} actionLabel="Discharge" actionHandler={() => dischargeBot(bot.id)} />
-//         ))}
-//         </div>
-//     )
+const MyBotArmy=({bots, dischargeBot})=>{
+    return(
+        <div className='your-bot-army'>
+            <h2>My Bot Army</h2>
+            {bots.map((bot)=>(
+        <BotCard key={bot.id} bot={bot} actionLabel="Discharge" actionHandler={() => dischargeBot(bot.id)} />
+        ))}
+        </div>
+    )
 
-// }
-// export default MyBotArmy;
+}
+export default MyBotArmy;
